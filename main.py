98d36c611f74e3da9eaaa177d597968e60fd698d
Rickobeat01 <ngeticheric28@gmail.com>
2026-07-16 11:57:10 +0300

    Add main function for car hire management system

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("=" * 50)
+    print(" SIALI MILES SAFARIS")
+    print(" CAR HIRE MANAGEMENT SYSTEM")
+    print("=" * 50)
+    print("System starting...")
+
+if name == "main":
+    main()
